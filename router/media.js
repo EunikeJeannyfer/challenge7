0f9d0ebb32fa9a68c47053aa47b873ec2c806832
Eunike Jeannyfer <ceunikejeannyfer@gmail.com>
2023-11-14 19:27:11 +0700

    imagekit

--- router/media.js
+++ router/media.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storage = require('../utils/multer')
 const controller = require('../app/controller')
+const multer = require('multer')();
 
 //agar bisa dibuka imagenya
 router.use('/images', express.static('public/images'));
@@ -18,4 +19,13 @@ router.post('/v2/upload/document',
         storage.document.single('document'),
         controller.media.uploadDocument)
 
+
+//QR-CODE
+router.post('/v2/qrcode', controller.media.qrcode)
+
+//imagekit
+router.post('/v2/upload/imagekit',
+        multer.single('image'),
+        controller.media.imagekitUpload
+)
 module.exports = router;
